10f0a001a0e807720e747cb7ffa50c0427ad307b
jorgep19 <jorgelpaez19@gmail.com>
2015-04-16 14:50:28 -0400

    fixed compilation issue

--- judgeServer/src/main/java/code_boss/ResultSender.java
+++ judgeServer/src/main/java/code_boss/ResultSender.java
@@ -10,30 +10,30 @@ import org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 
 public class ResultSender {
 
-    public void send(CodeEvaluation evaluation, String url) throws UnsupportedEncodingException {
+    public void send(CodeEvaluation evaluation, String url) {
         System.out.println(String.format("Sending result %s to %s", evaluation, url));
-
-        HttpClient httpclient = HttpClients.createDefault();
-        HttpPost post = new HttpPost(url);
-
-        // Request parameters and other properties.
-        StringEntity entityForPost = new StringEntity(evaluation.toJSON().toString());
-        post.setHeader("content-type", "application/json");
-        post.setEntity(entityForPost);
-
-        HttpResponse response;
         InputStream is = null;
+
         try {
-            //Execute and get the response.
-            response = httpclient.execute(post);
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-                is = entity.getContent();
-            }
+            HttpClient httpclient = HttpClients.createDefault();
+            HttpPost post = new HttpPost(url);
+
+            // Request parameters and other properties.
+            StringEntity entityForPost = new StringEntity(evaluation.toJSON().toString());
+            post.setHeader("content-type", "application/json");
+            post.setEntity(entityForPost);
+
+            HttpResponse response;
+
+                //Execute and get the response.
+                response = httpclient.execute(post);
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    is = entity.getContent();
+                }
 
         } catch (IOException e) {
             e.printStackTrace();
